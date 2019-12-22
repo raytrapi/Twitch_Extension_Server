90ec1771768f2cc6419b31a3298075cc0f8e2300
raytrapi <sdfenix@gmail.com>
2019-12-22 12:40:09 +0100

    Inicio del proyecto

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+use twitch\Token;
+use utilidades\Log;
+//include_once "utilidades\autoload.inc";
+include_once "utilidades/log.inc";
+include_once "twitch/twitch.php";
+try{
+   $datosUsuario=Token::usuarioID();
+   
+   if($datosUsuario===null){
+      return;
+   }
+   Log::debug($datosUsuario["id"]);
+}catch (\Exception $ex){
+   echo $ex->getMessage();
+}
+
+echo "#FF0000";
+
+
+
---
+++ twitch/twitch.php
@@ -0,0 +1,6 @@
+<?php 
+if (! defined("__DIR__")) {
+   define("__DIR__", dirname(__FILE__));
+}
+include_once __DIR__."/token.inc";
+include_once __DIR__."/utilidades/cors.inc";
